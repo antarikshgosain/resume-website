4ce6cf39255387fec88da57edb53b2c4a7b497a9
antarikshgosain <antariksh.gosain@gmail.com>
2024-03-17 03:22:06 -0400

    card colors added using dynamic enum class

---
+++ portfolio/src/app/_models/Tag.ts
@@ -0,0 +1,18 @@
+export class Tag {
+
+    static readonly ANGULAR = new Tag('Angular','red');
+    static readonly JAVA = new Tag('Java','orange');
+    static readonly REACT = new Tag('React','green');
+    static readonly AWS = new Tag('Amazon Web Services','blue');
+    static readonly PYTHON = new Tag('Python','yellow');
+    static readonly MS_AZURE = new Tag('Azure','darkblue');
+    static readonly SQL = new Tag('MySql','cyan');
+
+
+    private constructor(private readonly key: string, public readonly color: string){
+
+    }
+    toString(){
+        return this.key;
+    }
+}
--- portfolio/src/app/portfolio/portfolio.component.ts
+++ portfolio/src/app/portfolio/portfolio.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Project } from '../_models/project';
+import { Tag } from '../_models/Tag';
 
 @Component({
   selector: 'app-portfolio',
@@ -7,7 +9,19 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent {
+
+  project: Project = {
+    id: 0,
+    name: 'maple app',
+    summary: 'sweet and sour app',
+    description: '',
+    projectLink: '',
+    tags: [Tag.ANGULAR, Tag.JAVA],
+    pictures: [],
+  }
+
   constructor(private titleService: Title) {
     this.titleService.setTitle('Portfolio - Antariksh Gosain');
+    
   }
 }
